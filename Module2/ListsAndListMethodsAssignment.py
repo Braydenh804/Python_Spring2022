57e4697849262b2a1a355c1dfd901d086f0c4383
Brayden <braydenh804@gmail.com>
2023-01-24 17:01:22 -0600

    Initial commit

--- Module2/ListsAndListMethodsAssignment.py
+++ Module2/ListsAndListMethodsAssignment.py
@@ -2,7 +2,7 @@ MovieCharacter= "Leonardo DiCaprio in Inception"
 print(MovieCharacter.capitalize())
 print(MovieCharacter.find("Inception"))
 print(MovieCharacter.index("I"))
-print(MovieCharacter.isalpha())\]
+print(MovieCharacter.isalpha())
 print(MovieCharacter.isalnum())
 print(MovieCharacter.isdigit())
 print(MovieCharacter.islower())
