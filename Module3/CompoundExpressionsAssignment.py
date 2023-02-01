b6ac1964ba9ff7bf242a8457fc6a1309ce7803d6
Brayden <braydenh804@gmail.com>
2023-01-31 23:46:10 -0600

    Initial commit

---
+++ Module3/CompoundExpressionsAssignment.py
@@ -0,0 +1,12 @@
+MAX = 10
+MIN = 0
+y = 15
+
+if y > MAX:
+    print("y is greater than the MAX")
+elif y< MAX:
+    print("y is less than the MIN")
+x = 40
+if MIN < x < MAX:
+    print("y is inbetween MIN and MAX")
+else:
