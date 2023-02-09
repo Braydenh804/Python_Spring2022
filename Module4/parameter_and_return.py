a2aa893a76e5d6d15821a38ce714ca249c3a2524
Brayden <braydenh804@gmail.com>
2023-02-08 19:10:41 -0600

    Initial commit

--- Module4/parameter_and_return.py
+++ Module4/parameter_and_return.py
@@ -1,19 +1,23 @@
 def multiply_string(message, n):
-    print( "hi")
+    i = 0
+    while i < n:
+        print(message)
+        i += 1
 
 
 # Driver code
 if __name__ == "__main__":
     favorite_class = (input("Enter your favorite class: "))
     i = False
-        while not i:
-            try:
-                times_multiplied = (input("Enter how many times you want to multiply the string between 2 and 7: "))
-                times_multiplied_as_int = int(times_multiplied)
-                if 2 <= times_multiplied_as_int <= 7:
-                    i = True
-                else:
-                    raise ValueError
-            except:
-                print("Please input a valid number")
-     multiply_string(favorite_class, times_multiplied_as_int)
+    while not i:
+        try:
+            times_multiplied = (input("Enter a number between 2 and 7: "))
+            times_multiplied_as_int = int(times_multiplied)
+            if 2 <= times_multiplied_as_int <= 7:
+                i = True
+                multiply_string(favorite_class, times_multiplied_as_int)
+            else:
+                raise ValueError
+        except:
+            print('Please input a valid number')
+
