8db92b41b5b5d13057a7204e326b31c3b3bfc5aa
Braydenh804 <braydenh804@gmail.com>
2023-03-07 21:49:57 -0600

    Commit laptop

--- Module8/tests/test_average_scores.py
+++ Module8/tests/test_average_scores.py
@@ -5,12 +5,12 @@ from Module8.dictionary_update import average_scores
 class MyTestCase(unittest.TestCase):
 
     def test_average(self):
-        self.dictionary = {'score1': 80, 'score2': 90, 'score3': 100}
-        expected = 90
+        self.dictionary = {"Test 1": 31, "Test 2": 34, "Test 3": 54}
+        expected = 39.66666666
         # Act
         actual = average_scores(self.dictionary)
         # Assert
-        self.assertEqual(expected, actual)
+        self.assertAlmostEqual(expected, actual)
 
     def test_average_five(self):
         self.dictionary = {'score1': 80, 'score2': 90, 'score3': 100, 'score4': 85, 'score5': 75}
