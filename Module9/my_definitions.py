75dc1d5a8282d7bdad9a497b37ce0b11f5d246f8
Braydenh804 <braydenh804@gmail.com>
2023-03-22 19:43:51 -0500

    Commit laptop

---
+++ Module9/my_definitions.py
@@ -0,0 +1,13 @@
+def greeting():
+    print("Hello there! How are you doing today?")
+
+def message():
+    print("This code was written by Brayden.")
+
+def print_dict(dictionary):
+    for key, value in dictionary.items():
+        print(f"{key}: {value}")
+
+def print_set(set_object):
+    for item in set_object:
+        print(item)
---
+++ Module9/use_definitions_module.py
@@ -0,0 +1,15 @@
+import my_definitions as defs
+
+# Call the greeting function
+defs.greeting()
+
+# Call the message function
+defs.message()
+
+# Call the print_dict function with a dictionary
+my_dict = {"name": "Brayden", "age": 18, "city": "Pleasant Hill"}
+defs.print_dict(my_dict)
+
+# Call the print_set function with a set
+my_set = {"apple", "banana", "cherry"}
+defs.print_set(my_set)
