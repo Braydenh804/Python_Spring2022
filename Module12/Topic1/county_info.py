7568783d87aedee30076649554e1b8ebf4e4afd7
Braydenh804 <braydenh804@gmail.com>
2023-04-12 00:39:11 -0500

    Commit laptop

---
+++ Module12/Topic1/county_info.py
@@ -0,0 +1,7 @@
+class CountyInfo:
+    def __init__(self, per_capita_income, median_household_income, median_family_income, population, household_num):
+        self.per_capita_income = per_capita_income
+        self.median_household_income = median_household_income
+        self.median_family_income = median_family_income
+        self.population = population
+        self.household_num = household_num
